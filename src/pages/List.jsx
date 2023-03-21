c80ddb02c70f7f64d18ced5dcecb078114f985ca
Veeti <veeti.oliver@gmail.com>
2023-03-21 19:27:02 +0200

    Initial commit

---
+++ src/pages/List.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+
+export const List =() =>{
+    const [content, setContent] = useState([])
+    const [inputValue, setInputValue] = useState("");
+
+    return (
+        <div>
+            <div>
+                <form>
+                    <input 
+                        type="text"
+                        placeholder="Type here..."
+                        value={inputValue}
+                        required 
+                    />
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        </div>
+    )
+
+}
---
+++ src/pages/itemDatabase.js
@@ -0,0 +1,22 @@
+export const items = [
+    {   
+        img: 'https://anna.fi/awpo/img/s3/2020/12/18132531/joulupukki1260.jpg?s=a4d859edf4669bc3f1f59bb80717a603&fit=crop',
+        title: 'Item 1',        
+        description: 'Joulupukki',
+        id: 0,        
+    },
+
+    {   
+        img: 'https://cdn.pixabay.com/photo/2022/04/15/19/54/flowers-7135053_960_720.jpg',
+        title: 'Item 2',        
+        description: 'Kukka',
+        id: 1,        
+    },
+
+    {   
+        img: 'https://cdn.pixabay.com/photo/2022/01/31/12/46/bird-6983434_960_720.jpg',
+        title: 'Item 3',        
+        description: 'Lintu',
+        id: 2,        
+    },    
+]
