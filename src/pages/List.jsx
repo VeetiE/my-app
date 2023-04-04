3c3ea11f84b15971b98cfb7a18fd6f9ee8ec5504
Veeti <veeti.oliver@gmail.com>
2023-04-04 18:38:51 +0300

    Muutoksia listaolioon

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -5,17 +5,34 @@ export const List =() =>{
     const [content, setContent] = useState([])
     const [inputValue, setInputValue] = useState("");
 
+    const removeHandler = (removeIndex) => {
+        const removeItem = content.filter((item,index) => {
+            return removeIndex!== index;
+        });
+        setContent(removeItem);
+    }
+
+
     return (
         <div>
             <div>
-                <form>
+                <form onSubmit={(event) => {
+                    event.preventDefault()
+                    const temp = [inputValue, ...content];
+                    setContent(temp);
+                    setInputValue('')
+                }}>
                     <input 
                         type="text"
                         placeholder="Type here..."
                         value={inputValue}
+                        onChange={e => setInputValue(e.target.value)}
                         required 
                     />
                     <input type="submit" value="Submit"/>
+                {
+                    content.map((item, index)) 
+                }
                 </form>
             </div>
         </div>
