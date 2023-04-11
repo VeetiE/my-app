638b500c333bd3226611ebfe4c8e45de2bd73efd
Veeti <veeti.oliver@gmail.com>
2023-04-11 19:32:31 +0300

    Luotiin board ja square

--- src/tictactoe/TicTacToe.jsx
+++ src/tictactoe/TicTacToe.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './TicTacToe.css';
+import { Board } from "./Board";
 
 
 export const TicTacToe = () => {
     return (
         <div>
             <h1>Tic-Tac-Toe</h1>
+            <Board />
         </div>
     );
 }
