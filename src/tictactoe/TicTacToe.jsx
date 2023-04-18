6360da35547a2af4e0ea5071e25cba841fd4aba6
Veeti <veeti.oliver@gmail.com>
2023-04-18 19:31:02 +0300

    Saatiin peli toimivaksi

--- src/tictactoe/TicTacToe.jsx
+++ src/tictactoe/TicTacToe.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import './TicTacToe.css';
 import { Board } from "./Board";
+import { useState } from "react";
 
 
 export const TicTacToe = () => {
+    const initialBoard=['','','','','','','','',''];
+    const [gameState, setGameState] = useState(initialBoard);
+    const [isXTurn, setIsXturn] = useState(true);
+
+    const onSquareClick = (index) => {
+        let strings = Array.from(gameState)
+        if (strings[index] !== ''){
+            return;
+        }
+        strings[index] = isXTurn ? 'X' : 'O';
+        setGameState(strings);
+        setIsXturn(!isXTurn)
+    }
+
     return (
         <div>
-            <h1>Tic-Tac-Toe</h1>
-            <Board />
+            <div className="game">
+                <h1>Tic-Tac-Toe</h1>
+                <Board gameState={gameState} onSquareClick={onSquareClick}/>
+            </div>
         </div>
     );
 }
